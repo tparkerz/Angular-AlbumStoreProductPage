17b52943c71d2fb22596e67918eca33adf46b6b8
Priya sakthivelu <priya.sakthivelu@accenture.com>
2018-08-23 07:11:55 -0500

    Modified Observable

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
-import { Observalbe } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
